824186aa2039ebba2fe450ddeca05cd1ccb6a4c4
r0k04a4 <raj.kumar2@walmart.com>
2024-07-28 07:55:59 +0530

    3rd stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -6,6 +8,8 @@ public class Main {
   public static void main(String[] args){
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
+
+
         ServerSocket serverSocket = null;
         Socket clientSocket = null;
         int port = 6379;
@@ -14,9 +18,9 @@ public class Main {
           // Since the tester restarts your program quite often, setting SO_REUSEADDR
           // ensures that we don't run into 'Address already in use' errors
           serverSocket.setReuseAddress(true);
-          // Wait for connection from client.
-          clientSocket = serverSocket.accept();
-          clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
+            clientSocket = serverSocket.accept();
+            // Wait for connection from client.
+            process(clientSocket);
         } catch (IOException e) {
           System.out.println("IOException: " + e.getMessage());
         } finally {
@@ -29,4 +33,18 @@ public class Main {
           }
         }
   }
+
+    private static void process(Socket clientSocket) {
+        try (BufferedReader br = new BufferedReader(
+                new InputStreamReader(clientSocket.getInputStream()))) {
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                if (line.contains("PING")) {
+                    clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
