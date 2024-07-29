deb87cb52425a2a41f5488f43dc76e2be6c9d366
r0k04a4 <raj.kumar2@walmart.com>
2024-07-29 16:16:09 +0530

    Fixed

--- src/test/java/command/RedisCommandParserTest.java
+++ src/test/java/command/RedisCommandParserTest.java
@@ -7,9 +7,8 @@ public class RedisCommandParserTest {
 
     @Test
     public void parseAndExecute() {
-        RedisCommandParser parser = new RedisCommandParser();
         String commandStr = "*3\r\n$3\r\nSET\r\n$5\r\nmykey\r\n$7\r\nmyvalue\r\n";
-        String resp = parser.parseAndExecute(commandStr);
+        String resp = RedisCommandParser.parseAndExecute(commandStr);
         System.out.println(resp);
 
     }
