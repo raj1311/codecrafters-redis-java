ce381cb03a8733261f7152a88f9cd54f2c342f83
r0k04a4 <raj.kumar2@walmart.com>
2024-07-30 11:04:03 +0530

    CLI tests (cli-test-1722317643676)

--- src/main/java/command/GetCommand.java
+++ src/main/java/command/GetCommand.java
@@ -1,8 +1,11 @@
 package command;
 
+import java.util.List;
+
 public class GetCommand implements Command {
+
     @Override
-    public String execute(String[] args) {
+    public String execute(List<String> args) {
         return "";
     }
 }
