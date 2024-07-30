6674a89006c5ad35b3f22cd921ca0d976b582160
r0k04a4 <raj.kumar2@walmart.com>
2024-07-30 12:07:06 +0530

    CLI tests (cli-test-1722321426456)

--- src/main/java/command/GetCommand.java
+++ src/main/java/command/GetCommand.java
@@ -1,11 +1,22 @@
 package command;
 
+import repo.RedisStringRepoImpl;
+
 import java.util.List;
 
 public class GetCommand implements Command {
 
+    RedisStringRepoImpl redisRepo = RedisStringRepoImpl.getInstance();
     @Override
     public String execute(List<String> args) {
-        return "";
+        String resp = "-1";
+        if (args.size() != 2) {
+            return "-ERR wrong number of arguments for 'get' command\r\n";
+        }
+        if(redisRepo.isPresent(args.get(1))){
+            String value = redisRepo.findById(args.get(1));
+            resp = resp.replace("-1",value.length() + "\r\n" + value );
+        }
+        return "$" + resp + "\r\n";
     }
 }
