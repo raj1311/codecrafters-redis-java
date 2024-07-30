f35b4b02f120c13f88c453153b9ad5868f15c87e
r0k04a4 <raj.kumar2@walmart.com>
2024-07-30 10:55:55 +0530

    CLI tests (cli-test-1722317155220)

---
+++ src/main/java/model/CommandData.java
@@ -0,0 +1,20 @@
+package model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommandData {
+    private int totalArgs;
+    private String command;
+    private List<String> commandArgs = new ArrayList<>();
+}
