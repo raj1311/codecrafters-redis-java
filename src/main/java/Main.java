aabd93466b3d37a28d040cf0210ac48fa3aaf76d
r0k04a4 <raj.kumar2@walmart.com>
2024-07-30 11:05:29 +0530

    CLI tests (cli-test-1722317729069)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,7 @@
+import command.Command;
+import command.DataType;
 import command.RedisCommandParser;
+import model.CommandData;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,6 +10,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class Main {
   public static void main(String[] args){
@@ -24,32 +28,44 @@ public class Main {
           serverSocket.setReuseAddress(true);
             while (true) {
                 Socket clientSocket = serverSocket.accept();
-                executor.execute(() -> process(clientSocket));
+                try {
+                    String response = executor.submit(() -> process(clientSocket)).get();
+                    System.out.println(response);
+                    clientSocket.getOutputStream().write(response.getBytes());
+                }
+                catch (IOException e){
+                    e.printStackTrace();
+                }finally {
+                    try {
+                        if (clientSocket != null) {
+                            System.out.println("Closing connection");
+                            clientSocket.close();
+                        }
+                    } catch (IOException e) {
+                        System.out.println("IOException: " + e.getMessage());
+                    }
+                }
             }
             // Wait for connection from client.
-        } catch (IOException e) {
+        } catch (Exception e) {
           System.out.println("IOException: " + e.getMessage());
         }
+
   }
 
-    private static void process(Socket clientSocket) {
+    private static String process(Socket clientSocket) {
         try (BufferedReader br = new BufferedReader(
                 new InputStreamReader(clientSocket.getInputStream()))) {
             String line = null;
+            CommandData commandData = new CommandData();
             while ((line = br.readLine()) != null) {
-                String response = RedisCommandParser.parseAndExecute(line);
-                clientSocket.getOutputStream().write(response.getBytes());
+                RedisCommandParser.findDataTypeAndSetInfo(line,commandData);
             }
+            System.out.println("Command Data : " + commandData);
+            return RedisCommandParser.parseAndExecute(commandData);
         } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (clientSocket != null) {
-                    clientSocket.close();
-                }
-            } catch (IOException e) {
-                System.out.println("IOException: " + e.getMessage());
-            }
+            System.out.println("Got exception: " + e.getMessage());
         }
+        return "";
     }
 }
